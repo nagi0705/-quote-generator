a02f59b1e7bbfcaa625a1a3b9169787fcffee488
nagi0705 <bellreuben1126@gmail.com>
2025-01-11 20:16:37 +0900

    修正

--- server/api/quotes.get.ts
+++ server/api/quotes.get.ts
@@ -1,5 +1,7 @@
+import { defineEventHandler } from 'h3'
+
 export default defineEventHandler(() => {
-  return [
+  const quotes = [
     { text: "自分が嫌っている誰かは、影に潜んでいる自分の姿。", author: "ロコ" },
     { text: "心動かされるのは、全力を出し切った未完成。", author: "ノア" },
     { text: "答えは誰も教えてくれない。答えは1人1人違うから。", author: "きなこ" },
@@ -9,4 +11,5 @@ export default defineEventHandler(() => {
     { text: "失敗にするか、教訓にするかは、自分次第。", author: "寺田康佑" },
     { text: "外見は中身の一番外側。", author: "寺田凪沙" }
   ]
+  return quotes
 })
